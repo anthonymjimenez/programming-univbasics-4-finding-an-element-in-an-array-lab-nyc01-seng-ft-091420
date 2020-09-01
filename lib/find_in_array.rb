63e79e666070f930b18d4c09fe6945158e3da956
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-01 02:41:09 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   array.length.times do |x|
     if(array[x] === value_to_find)
-      return true
+      return x
     end
     return nil
   end
